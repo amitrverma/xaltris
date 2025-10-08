11609fd4afd40efd390c0c306b6cec927298fcc3
Amit R Verma <amit@amitrverma.com>
2025-10-08 11:11:22 +0530

    Posthog integration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
   output: 'standalone', // literal string, not general string
+
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 export default withMDX()(nextConfig)
